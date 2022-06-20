6852bf036d3ad66a2999b0c71cf2b3d5ded44d82
Claudius Holeksa <mail@keldu.de>
2022-06-20 11:43:05 +0200

    reason why I do not like the name in the descriptor itself

--- mem_back/var_descriptor.h
+++ mem_back/var_descriptor.h
@@ -5,6 +5,15 @@
 #include <cstdint>
 
 namespace imb {
+/**
+ * Personally I don't like the string here.
+ * The only thing that describes a type is the string.
+ * As far as I can see the other ids just describe positional data.
+ * So I think it should be sorted out earlier.
+ *
+ * For example by grabbing the registry with the string and then accessing the remaining part with the ids
+ * Should be faster as well.
+ */
 class var_descriptor {
 private:
 	std::string name;
