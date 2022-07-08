5c041d1471cde0cfc9c0cc9682078eb492444189
keldu <mail@keldu.de>
2022-07-08 10:55:15 +0200

    minor bump

--- mem_back/cuda/registry.h
+++ mem_back/cuda/registry.h
@@ -2,18 +2,51 @@
 
 #include "../static/registry.h"
 
+
+
 namespace imb {
 namespace storage {
 struct cuda {};
 }
 template<typename DataDescriptor>
-class registry_impl<DataDescriptor,storage::cuda> : public registry<DataDescriptor>{
+class registry<DataDescriptor,storage::cuda> : public i_registry<DataDescriptor>{
 public:
 	registry() = default;
 
-
-	var_collection<DataDescriptor, storage::cuda> global_collection() {
-		
+	var_collection<DataDescriptor> global_collection() override {
+		return {*this};
 	}
+
+	DataDescriptor& at(size_t i) override {
+
+	}
+public:
+	struct allocator {
+		using value_type = DataDescriptor;
+
+		value_type* allocate(size_t len){
+			void* ptr{};
+
+			cudaMalloc(&ptr, len);
+
+			return ptr;
+		}
+
+		void deallocate(value_type* buffer, size_t len){
+			if(!buffer){
+				return;
+			}
+
+			cu
+		}
+	};
+private:
+
+	std::vector<DataDescriptor,allocator> data;
+};
+
+template<typename Key, typename DataDescriptor>
+class registry_index<Key,DataDescriptor, storage::cuda> : public i_registry_index<Key, DataDescriptor> {
+public:
 };
 }
