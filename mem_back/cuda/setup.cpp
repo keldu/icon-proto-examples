291bb8a6c98db668c96ba75a54b0654a4dd0f979
Claudius Holeksa <mail@keldu.de>
2022-07-14 13:08:33 +0200

    basic test

--- mem_back/cuda/setup.cpp
+++ mem_back/cuda/setup.cpp
@@ -1,11 +1,26 @@
 #include "./registry.h"
 
-using cuda_registry = imb::registry<uint64_t,imb::storage::cuda>;
+#include <iostream>
+
+using cuda_basic_registry = imb::registry<uint64_t,imb::storage::cuda>;
 
 int main(){
 	using namespace imb;
 
-	cuda_registry cuda_reg;
+	cuda_basic_registry cuda_reg;
+
+	auto builder = create_collection_builder<uint64_t>(cuda_reg);
+
+	builder.add_var(2);
+	builder.add_var(5);
+	builder.add_var(20);
+	builder.add_var(13123);
+
+	auto collection = builder.build();
+
+	collection.for_each([](uint64_t value){
+		std::cout<<"Value: \t"<<value<<"\n";
+	});
 
 	return 0;
 }
