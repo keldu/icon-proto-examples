b106aac1e0e43d77ee24254042ab8143ef978538
Claudius Holeksa <mail@keldu.de>
2022-07-14 13:02:00 +0200

    basic check

--- mem_back/cuda/setup.cpp
+++ mem_back/cuda/setup.cpp
@@ -1,5 +1,11 @@
 #include "./registry.h"
 
+using cuda_registry = imb::registry_tuple<imb::storage::cuda, uint64_t>;
+
 int main(){
+	using namespace imb;
+
+	cuda_registry cuda_reg;
+
 	return 0;
 }
