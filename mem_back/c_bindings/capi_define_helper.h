133d65c44d62026e7288ac5b13e1c3c4cf6d0f60
Claudius Holeksa <mail@keldu.de>
2022-08-05 16:20:56 +0200

    adding macro exp problems

--- mem_back/c_bindings/capi_define_helper.h
+++ mem_back/c_bindings/capi_define_helper.h
@@ -10,17 +10,17 @@ extern "C"{
 }
 
 #define IMB_CAPI_TYPE_IMPL(TYPE) \
-int var_collection_create_##TYPE ( struct var_collection_##TYPE* collection, size_t size){ \
+int var_collection_create_##TYPE ( struct var_collection_##TYPE* collection, struct registry_##TYPE* registry){ \
 	if(collection == 0L){\
 		return -1;\
 	}\
 \
-	void* ptr = malloc(sizeof(size_t) * size);\
+	void* ptr = malloc(sizeof(size_t));\
 	if(ptr) {\
 		return -1;\
 	}\
 	collection->ids = (size_t*)ptr;\
-	collection->size = size; \
+	collection->size = 0; \
 	return 0;\
 }\
 \
@@ -29,7 +29,7 @@ void var_collection_destroy_##TYPE ( struct var_collection_##TYPE* collection) {
 } \
 \
 int registry_create_##TYPE ( struct registry_##TYPE* registry) { \
-	void* ptr = malloc(sizeof(imb::registry<float>)); \
+	void* ptr = malloc(sizeof(imb::registry< TYPE >)); \
 	if(!ptr){ \
 		return -1; \
 	} \
@@ -42,6 +42,17 @@ void registry_destroy_##TYPE ( struct registry_##TYPE* registry) { \
 		return; \
 	} \
 	free(registry->internal_ptr); \
+} \
+\
+size_t registry_add_var_##TYPE ( struct registry_##TYPE* registry, ##TYPE variable) { \
+	if(! registry) { \
+		return 0; \
+	} \
+	if(! registry->internal_ptr){ \
+		return 0; \
+	} \
+	auto* intern = static_cast<imb::registry< TYPE >*>(registry->internal_ptr); \
+	return intern->add_var(variable); \
 }
 
 #define IMB_CAPI_TYPE_HEADERS(TYPE) \
@@ -51,10 +62,11 @@ struct registry_##TYPE { \
 \
 extern int registry_create_##TYPE ( struct registry_##TYPE * collection); \
 extern void registry_destroy_##TYPE ( struct registry_##TYPE * collection); \
+extern size_t registry_add_var_##TYPE ( struct registry_##TYPE* collection); \
 \
 struct var_collection_##TYPE { \
 	size_t size; \
 	size_t* ids; \
 }; \
-extern int var_collection_create_##TYPE ( struct var_collection_##TYPE* collection, size_t size); \
+extern int var_collection_create_##TYPE ( struct var_collection_##TYPE* collection, struct registry_##TYPE* registry); \
 extern void var_collection_destroy_##TYPE ( struct var_collection_##TYPE* collection);
