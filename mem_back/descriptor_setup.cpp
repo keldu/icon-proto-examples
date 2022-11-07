0f82f581e9c0166ac4a11d49d76ac037e2753208
Claudius Holeksa <mail@keldu.de>
2022-11-07 14:23:17 +0100

    dangling things moved to jinja2

--- mem_back/descriptor_setup.cpp
+++ mem_back/descriptor_setup.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 
 void simple_setup(){
+	std::cout<<"\n## Simple setup ##\n"<<std::endl;
 	imb::descriptor_registry_map<float, int32_t> map;
 
 	map.template add_registry<float>("temperature");
@@ -45,6 +46,28 @@ void simple_setup(){
 		std::cout.flush();
 	}
 
+	map.template add_registry<int32_t>("some_sort_of_id");
+
+	{
+		auto reg_ptr = map.template find_registry<int32_t>("some_sort_of_id");
+		if(!reg_ptr){
+			return;
+		}
+
+		auto& registry = *reg_ptr;
+		auto builder = imb::create_keyed_collection_builder<imb::var_location, int32_t>(registry);
+
+		builder.add_var({1,1,1,1}, 2);
+		builder.add_var({1,1,1,2}, 3);
+		builder.add_var({1,1,1,4}, 4);
+		builder.add_var({1,1,1,5}, 5);
+		builder.add_var({1,1,1,6}, 6);
+		builder.add_var({1,1,2,1}, 7);
+		builder.add_var({1,1,3,1}, 8);
+	}
+	
+	std::cout<<"\n## Variant setup ##\n"<<std::endl;
+
 	{
 		auto var_col = map.global_collection_variant();
 
@@ -67,9 +90,9 @@ void simple_setup(){
 }
 
 int main(){
-	std::cout<<"\n## Simple setup ##\n"<<std::endl;
 
 	simple_setup();
+	
 
 	return 0;
 }
--- mem_back/shell.nix
+++ mem_back/shell.nix
@@ -1,4 +1,4 @@
 { pkgs ? import <nixpkgs> {} }:
   pkgs.mkShellNoCC {
-    nativeBuildInputs = [ pkgs.gnumake pkgs.gfortran12 pkgs.python3 ];
+    nativeBuildInputs = [ pkgs.gnumake pkgs.gfortran12 pkgs.python39 pkgs.python39Packages.jinja2 ];
   }
