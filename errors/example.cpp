9da379ef587067c2d02acf7eabfdf61f8b018695
Claudius Holeksa <mail@keldu.de>
2022-12-13 14:08:54 +0100

    added third query

--- errors/example.cpp
+++ errors/example.cpp
@@ -11,9 +11,11 @@ int main(){
 
 	auto id = error_registry::template get_or_new_error_id <FooError> ("Foo", true);
 	auto id2 = error_registry::template get_or_new_error_id <BarError> ("Bar", true);
+	auto id3 = error_registry::template get_or_new_error_id <FooError> ("Foo", true);
 
 	std::cout << "Foo: " << id << std::endl;
 	std::cout << "Bar: " << id2 << std::endl;
+	std::cout << "Foo: " << id3 << std::endl;
 
 	return 0;
 }
